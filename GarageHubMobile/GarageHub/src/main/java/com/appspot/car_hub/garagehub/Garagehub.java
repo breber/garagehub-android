88a3a4bde3a34a023f9f0c7dab4b1e917837045d
Brian Reber <reber.brian@gmail.com>
2017-11-24 22:58:36 -0600

    Use correct URL

--- GarageHubMobile/GarageHub/src/main/java/com/appspot/car_hub/garagehub/Garagehub.java
+++ GarageHubMobile/GarageHub/src/main/java/com/appspot/car_hub/garagehub/Garagehub.java
@@ -57,7 +57,7 @@ public class Garagehub extends com.google.api.client.googleapis.services.json.Ab
    *
    * @since 1.7
    */
-  public static final String DEFAULT_ROOT_URL = "https://car-hub.appspot.com/_ah/api/";
+  public static final String DEFAULT_ROOT_URL = "https://default-dot-car-hub.appspot.com/_ah/api/";
 
   /**
    * The default encoded service path of the service. This is determined when the library is
